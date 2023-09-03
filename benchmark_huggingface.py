85fbfdd4421646638527d4afa29368fb062ff435
sfc-gh-zhwang <flex.wang@snowflake.com>
2023-09-03 15:45:38 -0700

    commit

---
+++ benchmark_huggingface.py
@@ -0,0 +1,57 @@
+import numpy as np
+from transformers import AutoTokenizer, AutoModelForCausalLM, TextStreamer
+import time
+import argparse
+import torch
+
+
+def warmup(model, tokenizer):
+    input = ['']*10
+    tokens = tokenizer(input, return_tensors='pt')
+    tokens = tokens.to('cuda')
+    model.generate(**tokens)
+
+
+def benchmark_huggingface(
+    model_path,
+    max_output_len,
+    batch_size,
+    streaming=False):
+    model = AutoModelForCausalLM.from_pretrained(model_path,
+                                                 device_map='auto',
+                                                 torch_dtype=torch.float16)
+    print(f'model intialized in {model.device}')
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+    print('warm up')
+    warmup(model=model, tokenizer=tokenizer)
+    print('warm up done')
+    print('start benchmarking')
+    start_time = time.time()
+    input = ['']*batch_size
+    tokens = tokenizer(input, return_tensors='pt')
+    tokens = tokens.to('cuda')
+    if streaming:
+        pass
+    else:
+        new_tokens = model.generate(**tokens, max_new_tokens=max_output_len)
+        print('generate done')
+        for t in new_tokens:
+            tokenizer.decode(t, skip_special_tokens=True)
+        print('tokenizer done')
+        end_time = time.time()
+        print('latency: ', end_time - start_time)
+
+
+parser = argparse.ArgumentParser(description="Benchmark")
+
+# Add arguments to the parser
+parser.add_argument("--model_path", type=str, default='/notebooks/llama2-7B-hf')
+parser.add_argument("--batch_size", type=int, default=1)
+parser.add_argument("--max_output_len", type=int, default=32)
+
+# Parse the command-line arguments
+args = parser.parse_args()
+
+benchmark_huggingface(model_path=args.model_path,
+                      max_output_len=args.max_output_len,
+                      batch_size=args.batch_size)
---
+++ benchmark_triton.py
@@ -0,0 +1,82 @@
+import numpy as np
+import tritonclient.grpc as grpcclient
+from tritonclient.utils import *
+import multiprocessing as mp
+from transformers import AutoTokenizer
+import time
+from functools import partial
+import argparse
+
+def _input(name: str, data: np.ndarray) -> grpcclient.InferInput:
+    t = grpcclient.InferInput(name, data.shape, np_to_triton_dtype(data.dtype))
+    t.set_data_from_numpy(data)
+    return t
+
+
+start_time = None
+first_token_time = None
+end_time = None
+output = None
+printed = ''
+
+def stream_callback(a, result, error):
+    # print('stream_callback')
+    global first_token_time
+    global end_time
+    global output
+    global printed
+    if error:
+        raise error
+    end_time = time.time()
+    if first_token_time is None:
+        first_token_time = end_time
+    output = result.as_numpy('output')
+    decoded = output[0][0].decode()
+    print(decoded[len(printed):], end='')
+    printed=decoded
+
+
+def benchmark_triton(
+    model_name,
+    tokenizer_path,
+    max_output_len,
+    batch_size,
+    addr = "localhost:8001"
+    ):
+    with grpcclient.InferenceServerClient(addr, verbose=False) as client:
+        result_queue = mp.Queue()
+        inputs = [
+            _input("text", np.array([''] * batch_size, dtype=object).reshape(-1, 1)),
+            _input("max_output_len", np.array([[max_output_len]]*batch_size, dtype=np.int32))
+        ]
+        start_time = time.time()
+        client.start_stream(callback=partial(stream_callback, result_queue))
+        client.async_stream_infer(model_name, inputs)
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
+
+    first_token_latency = first_token_time - start_time
+    total_duration = end_time - start_time
+    streaming_duration = end_time - first_token_time
+    tokens = 0
+    for i in output:
+        tokens += len(tokenizer.encode(i[0].decode())) - 1 # get rid of the start token.
+
+    print('\nfirst_token_latency: ', first_token_latency)
+    print('total duration', total_duration)
+    print('total tokens generated: ', tokens, 'throughput', tokens/streaming_duration)
+
+parser = argparse.ArgumentParser(description="Benchmark")
+
+# Add arguments to the parser
+parser.add_argument("--model_name", type=str, default='llama-2-70b-chat-hf-ft-streaming')
+parser.add_argument("--tokenizer_path", type=str, default='/models/triton/llama-2-70b-chat-hf-ft-streaming_tokenizer/1/')
+parser.add_argument("--batch_size", type=int, default=1)
+parser.add_argument("--max_output_len", type=int, default=32)
+
+# Parse the command-line arguments
+args = parser.parse_args()
+
+benchmark_triton(model_name=args.model_name,
+                 tokenizer_path=args.tokenizer_path,
+                 max_output_len=args.max_output_len,
+                 batch_size=args.batch_size)
