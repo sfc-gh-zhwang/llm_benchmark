c5c489c9e21c85f3d98d338c967afd2cb1c4d888
sfc-gh-zhwang <flex.wang@snowflake.com>
2023-09-07 21:44:39 -0700

    commit

--- benchmark_vllm.py
+++ benchmark_vllm.py
@@ -1,6 +1,7 @@
 # Modify from https://github.com/vllm-project/vllm/blob/852ef5b4f5481ce526c804ea234d1de0df91f48d/benchmarks/benchmark_throughput.py
 import argparse
 import time
+import random
 
 from transformers import AutoTokenizer
 from vllm import LLM, SamplingParams
@@ -9,6 +10,8 @@ long_sentence = "A path from a point approximately 330 metres east of the most s
     "183 metres east-south-east of the most easterly corner of Unit 5 Foundry Industrial Estate, Victoria Street and approximately 272 metres east of the most north-easterly corner of 26 Ann Street West, Widnes, then proceeding in a generally north easterly direction for approximately 58 metres to a point approximately 216 metres east-south-east of the most easterly corner of Unit 4 Foundry Industrial Estate, Victoria Street and approximately 221 metres east of the most southerly corner of Unit 5 Foundry Industrial Estate, Victoria Street, then proceeding in a generally easterly direction for approximately 45 metres to a point approximately 265 metres east-south-east of the most north-easterly corner of Unit 3 Foundry Industrial Estate, Victoria Street and approximately 265 metres east of the most southerly corner of Unit 5 Foundry Industrial Estate, Victoria Street, then proceeding in a generally east-south-east direction for approximately 102 metres to a point approximately 366 metres east-south-east of the most easterly corner of Unit 3 Foundry Industrial Estate, Victoria Street and approximately 463 metres east of the most north easterly corner of 22 Ann Street West, Widnes, then proceeding in a generally north-north-easterly direction for approximately 19 metres to a point approximately 368 metres east-south-east of the most easterly corner of Unit 3 Foundry Industrial Estate, Victoria Street and approximately 512 metres east of the most south easterly corner of 17 Batherton Close, Widnes then proceeding in a generally east-south, easterly direction for approximately 16 metres to a point approximately 420 metres east-south-east of the " \
     "most southerly corner of Unit 2 Foundry Industrial Estate, Victoria Street and approximately 533 metres east of the most south-easterly corner of 17 Batherton Close, then proceeding in a generally east-north-easterly direction for approximately 240 metres to a point approximately 606 metres east of the most northerly corner of Unit 4 Foundry Industrial Estate, Victoria Street and approximately 23 metres south of the most south westerly corner of the boundary fencing of the scrap metal yard on the south side of Cornubia Road, Widnes, then proceeding in a generally northern direction for approximately 44 metres to a point approximately 3 metres west-north-west of the most north westerly corner of the boundary fence of the scrap metal yard on the south side of Cornubia Road and approximately 47 metres west-south-west of the stub end of Cornubia Road."
 
+words = long_sentence.split(' ')
+num_words = len(words)
 
 def warmup(llm):
     for i in range(10):
@@ -30,6 +33,22 @@ def warmup(llm):
     llm._run_engine(use_tqdm=True)
 
 
+# generate a random sentence with token number = token_num
+def generate_input(tokenizer, token_num):
+    if token_num <= 1:
+        return ''
+    sentence = ''
+    for i in range(token_num * 3):
+        word_index = random.randint(0, num_words-1)
+        word = words[word_index]
+        if sentence == '':
+            sentence = word
+        else:
+            sentence += ' ' + word
+    tokens = tokenizer(sentence)['input_ids'][:token_num]
+    return tokenizer.decode(tokens, skip_special_tokens=True)
+
+
 def benchmark_vllm(
     model_path,
     max_output_len,
@@ -46,12 +65,8 @@ def benchmark_vllm(
     warmup(llm)
     print('done warm up')
 
-    if input_len == 1:
-        input = ''
-    else:
-        tokens = tokenizer(long_sentence)['input_ids'][:input_len]
-        input = tokenizer.decode(tokens)
-        print('input token len: ', len(tokens))
+    # To make sure the rng is deterministic
+    random.seed(42)
     # Add the requests to the engine.
     for i in range(batch_size):
         sampling_params = SamplingParams(
@@ -64,7 +79,7 @@ def benchmark_vllm(
         )
         # FIXME(woosuk): Do not use internal method.
         llm._add_request(
-            prompt=input,
+            prompt=generate_input(tokenizer=tokenizer, token_num=input_len),
             prompt_token_ids=None,
             sampling_params=sampling_params,
         )
