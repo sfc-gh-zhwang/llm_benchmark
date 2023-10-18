fc619dd847c217b5e1c3b61e18fd0e39f2ff3899
sfc-gh-zhwang <flex.wang@snowflake.com>
2023-10-17 21:52:25 -0700

    commit

--- benchmark_triton.py
+++ benchmark_triton.py
@@ -76,6 +76,8 @@ def benchmark_triton(
         ):
     tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
     prompts = generate_inputs(tokenizer, input_len, batch_size)
+    print(prompts)
+    exit()
     inputs = [
         _input("text", np.array(prompts, dtype=object).reshape(-1, 1)),
         _input("max_output_len", np.array([[max_output_len]]*batch_size, dtype=np.int32)),
