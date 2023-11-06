1b3fd4e6a0286d3bc9d0fbdd7f084404feee55d7
sfc-gh-zhwang <flex.wang@snowflake.com>
2023-11-05 17:36:35 -0800

    commit

--- benchmark_triton.py
+++ benchmark_triton.py
@@ -51,9 +51,8 @@ output = None
 start_time = None
 output_tokens = None
 end_time = None
-output_tokens = None
 
-def start_stream(addr, tokenizer, model_name, inputs, index):
+def start_stream(addr, input_len, tokenizer, model_name, inputs, index):
     print("start_stream index:", index)
     with grpcclient.InferenceServerClient(addr, verbose=False) as client:
         first_token_time[index] = None
@@ -61,13 +60,12 @@ def start_stream(addr, tokenizer, model_name, inputs, index):
         client.start_stream(callback=partial(stream_callback, index))
         client.async_stream_infer(model_name, inputs)
 
-    global end_time, output_tokens
+    global end_time
     first_token_latency[index] = first_token_time[index] - start_time[index]
     latency[index] = end_time[index] - start_time[index]
     tokens = 0
     for ot in output[index]:
         output_len = len(tokenizer.encode(ot[0].decode())) - 1
-        output_tokens += output_len
         tokens += input_len + output_len    # get rid of the start token.
     throughput[index] = tokens/latency[index]
 
@@ -106,6 +104,7 @@ def benchmark_triton(
                 start_stream(addr=addr,
                              model_name=model_name,
                              inputs=inputs,
+                             input_len=input_len,
                              tokenizer=tokenizer,
                              index=i*parallelism+p)
             for ot in output:
