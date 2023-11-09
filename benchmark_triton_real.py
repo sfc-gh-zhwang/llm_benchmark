e4d76f81fa2acf4d010457c9e0da8bf45205bb97
sfc-gh-zhwang <flex.wang@snowflake.com>
2023-11-09 12:07:17 -0800

    commit

--- benchmark_triton_real.py
+++ benchmark_triton_real.py
@@ -40,7 +40,10 @@ def send_batch(client, model_name, n_requests, batch_size, max_output_len):
             _input("max_output_len", np.array([[max_output_len]]*len(batch), dtype=np.int32))
         ]
         resp = client.infer(model_name, inputs)
-        print(resp.as_numpy("output"))
+        input_sequence_lengths = resp.as_numpy('input_sequence_lengths')
+        output_sequence_lengths = resp.as_numpy('output_sequence_lengths')
+        print(input_sequence_lengths)
+        print(output_sequence_lengths)
 
 
 def benchmark_triton_real(
