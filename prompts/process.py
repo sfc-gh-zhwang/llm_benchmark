6fe9281a0d2401a594cde732ab22a135d202fe5d
sfc-gh-zhwang <flex.wang@snowflake.com>
2023-11-11 21:18:28 -0800

    commit

--- prompts/process.py
+++ prompts/process.py
@@ -1,7 +1,7 @@
 import csv
 
 from datasets import load_dataset
-from utils import calculate_stats
+#from utils import calculate_stats
 
 ds = load_dataset("ccdv/arxiv-summarization")
 print(ds)
@@ -15,14 +15,20 @@ for i in ds['test']:
     l1.append(len(i['article']))
     l2.append(len(i['abstract']))
     id += 1
-    print(i['abstract'])
-    data.append([i['abstract']])
-    if id >= 2048:
+    article = i['article'].split()
+    article = ' '.join(article[:2000])
+    data.append([article])
+    if id >= 1024:
         break
 with open('prompts/arxiv.csv', 'w', newline='') as csvfile:
     csv_writer = csv.writer(csvfile)
     csv_writer.writerows(data)
 
+def calculate_stats(lt):
+    _max = max(lt)
+    _min = min(lt)
+    mean = sum(lt)//len(lt)
+    return f'min: {_min}, max: {_max}, mean: {mean}'
 
 print(calculate_stats(l1))
 print(calculate_stats(l2))
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import csv
 
 import numpy as np
+import math
 from scipy import stats
 
 
@@ -19,13 +20,34 @@ def generate_inputs(tokenizer, token_num, batch_size):
     return [generate_input(tokenizer, token_num) for _ in range(batch_size)]
 
 
-def get_prompts(n):
+# def get_prompts(n):
+#     prompts = []
+#     with open('prompts/prompts2048.csv', newline='') as file:
+#         reader = csv.reader(file)
+#         for row in reader:
+#             prompts.append(row[0])
+#     return prompts[:n]
+
+def shrink_sentence(s, r):
+    words = s.split()
+    n = math.ceil(len(words) * r)
+    return ' '.join(words[:n])
+
+
+# r=0.3775 -> mean tokens 1024
+# r=0.179 -> mean tokens 512
+# r=0.079 -> mean tokens 256
+def get_prompts(n, r=1.0):
     prompts = []
-    with open('prompts/prompts2048.csv', newline='') as file:
+    with open('prompts/arxiv.csv', newline='') as file:
         reader = csv.reader(file)
         for row in reader:
-            prompts.append(row[0])
-    return prompts[:n]
+            prompt = f'''<s>[INST]<<SYS>>
+    Please summarize the text that is given. Return just the summary and no additional conversational dialog such as ""Sure, here is the summary of the text:"".
+    <</SYS>> {shrink_sentence(row[0], r)}[/INST]'''
+            prompts.append(prompt)
+    return prompts
+
 
 def calculate_mean(data, confidence_level=0.95):
     # Calculate the sample statistics
