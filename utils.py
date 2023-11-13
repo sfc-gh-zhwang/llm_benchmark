30ac2648764b0001dc7f169dea952767fc069748
sfc-gh-zhwang <flex.wang@snowflake.com>
2023-11-12 17:37:56 -0800

    commit

--- utils.py
+++ utils.py
@@ -37,7 +37,7 @@ def shrink_sentence(s, r):
 # r=0.3775 -> mean tokens 1024
 # r=0.179 -> mean tokens 512
 # r=0.079 -> mean tokens 256
-def get_prompts(n, r=0.3775):
+def get_prompts(n, r=0.179):
     prompts = []
     with open('prompts/arxiv.csv', newline='') as file:
         reader = csv.reader(file)
