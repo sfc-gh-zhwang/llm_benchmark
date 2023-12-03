f8b8f405e9104f08bee4df853ecbcac3a37e0f07
sfc-gh-zhwang <flex.wang@snowflake.com>
2023-12-02 22:56:59 -0800

    commit

--- new/benchmark_batched_inference.py
+++ new/benchmark_batched_inference.py
@@ -1,4 +1,3 @@
-from vllm import LLM, SamplingParams
 from prompt_generator import PromptsGenerator
 import argparse
 
@@ -21,12 +20,37 @@ def parse_args():
                         default=32)
     parser.add_argument("-l",
                         "--prompt_length",
+                        help="average number of tokens each prompt.",
                         type=int,
                         default=1024)
     parser.add_argument('--framework', required=True, choices=['vllm', 'deepspeed', 'trtllm'])
+    parser.add_argument("-tp",
+                        "--tensor_para",
+                        type=int,
+                        help="Tensor parallelism",
+                        default=1)
+    parser.add_argument('--model', required=True, help="path to the model")
 
     args = parser.parse_args()
     return args
+
+
+def benchmark_vllm(model, tp, num_queries, warmup, prompt_length, max_new_tokens):
+    from vllm import LLM, SamplingParams
+
+    # Create an LLM.
+    llm = LLM(model=model_path, tensor_parallel_size=tp)
+
+    print('warmming up...')
+    warmup_prompts = prompt_generator.generate(1024, 1024*0.3, 4096-1024, warmup, show_progress=True)
+    llm.generate(warmup_prompts)
+    print('warm up finished')
+
+    # Create a sampling params object.
+    sampling_params = SamplingParams(temperature=0, max_tokens=max_new_tokens)
+    outputs = llm.generate(prompts, sampling_params)
+    for output in outputs:
+        print(output)
 
 
 if __name__ == "__main__":
@@ -36,23 +60,28 @@ if __name__ == "__main__":
         print('{}: {}'.format(key, vars(args)[key]))
     print('========================================')
 
+    if args.framework == 'vllm':
+        benchmark_vllm(
+            model=args.model,
+            tp=args.tensor_para,
+            num_queries=args.num_queries,
+            warmup=args.warmup,
+            prompt_length=args.prompt_length,
+            max_new_tokens=args.max_new_tokens)
 
-# Sample prompts.
-model_path = "/models/llama-2-7b-chat-hf/"
-prompt_generator = PromptsGenerator(tokenizer_path=model_path)
 
-prompts = prompt_generator.generate(1024, 1024*0.3, 4096-1024, 32, show_progress=True)
+# # Sample prompts.
+# model_path = "/models/llama-2-7b-chat-hf/"
+# prompt_generator = PromptsGenerator(tokenizer_path=model_path)
 
-# Create a sampling params object.
-sampling_params = SamplingParams(temperature=0, max_tokens=1024)
+# prompts = prompt_generator.generate(1024, 1024*0.3, 4096-1024, 32, show_progress=True)
 
-# Create an LLM.
-llm = LLM(model=model_path)
-# Generate texts from the prompts. The output is a list of RequestOutput objects
-# that contain the prompt, generated text, and other information.
-outputs = llm.generate(prompts, sampling_params)
-# Print the outputs.
-for output in outputs:
-    prompt = output.prompt
-    generated_text = output.outputs[0].text
-    print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
+
+# # Generate texts from the prompts. The output is a list of RequestOutput objects
+# # that contain the prompt, generated text, and other information.
+# outputs = llm.generate(prompts, sampling_params)
+# # Print the outputs.
+# for output in outputs:
+#     prompt = output.prompt
+#     generated_text = output.outputs[0].text
+#     print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
