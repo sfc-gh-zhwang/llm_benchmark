011f7073a099b1fff5cde827ebdaa60942d25723
sfc-gh-zhwang <flex.wang@snowflake.com>
2023-12-02 21:55:18 -0800

    commit

--- new/prompts.py
+++ new/prompts.py
@@ -43,10 +43,11 @@ Please summarize the text that is given. Return just the summary and no addition
 
 tokenizer = AutoTokenizer.from_pretrained('/models/llama-2-7b-chat-hf')
 pg = PromptsGenerator(None)
-prompts = pg.generate(1024, 1024*0.3, 4096-1024, 1024)
+prompts = pg.generate(1024, 1024*0.3, 4096-1024, 32)
 
 l = []
 for i in prompts:
+    print(i)
     l.append(len(tokenizer.encode(i)))
 
 print(len(l), max(l), min(l), sum(l)/len(l))
