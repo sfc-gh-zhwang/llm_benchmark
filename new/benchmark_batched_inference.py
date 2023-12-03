fd7743a2166aba55ffc5c8e1a98ab7bc5683ad3d
sfc-gh-zhwang <flex.wang@snowflake.com>
2023-12-02 22:59:54 -0800

    commit

--- new/benchmark_batched_inference.py
+++ new/benchmark_batched_inference.py
@@ -1,6 +1,8 @@
 from prompt_generator import PromptsGenerator
 import argparse
 
+LLAMA_MAX_SEQUENCE_LENGTH = 4096
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Benchmark inference")
@@ -39,15 +41,19 @@ def benchmark_vllm(model, tp, num_queries, warmup, prompt_length, max_new_tokens
     from vllm import LLM, SamplingParams
 
     # Create an LLM.
-    llm = LLM(model=model_path, tensor_parallel_size=tp)
-
-    print('warmming up...')
-    warmup_prompts = prompt_generator.generate(1024, 1024*0.3, 4096-1024, warmup, show_progress=True)
-    llm.generate(warmup_prompts)
-    print('warm up finished')
-
+    llm = LLM(model=model, tensor_parallel_size=tp)
+    
     # Create a sampling params object.
     sampling_params = SamplingParams(temperature=0, max_tokens=max_new_tokens)
+
+    prompt_generator = PromptsGenerator(tokenizer_path=model)
+    if warmup > 0:
+        print('warmming up...')
+        warmup_prompts = prompt_generator.generate(1024, 1024*0.3, 2048, warmup, show_progress=True)
+        llm.generate(warmup_prompts, sampling_params)
+        print('warm up finished')
+
+    prompts = prompt_generator.generate(prompt_length, prompt_length*0.3, LLAMA_MAX_SEQUENCE_LENGTH-max_new_tokens, num_queries, show_progress=True)
     outputs = llm.generate(prompts, sampling_params)
     for output in outputs:
         print(output)
