70b05c1fb87aa4d95e85304c6872d3177cfd3a55
sfc-gh-zhwang <flex.wang@snowflake.com>
2023-12-04 21:24:58 -0800

    commit

--- new/tensorrt_llm_llama.py
+++ new/tensorrt_llm_llama.py
@@ -2,7 +2,7 @@ import numpy as np
 import tritonclient.grpc as grpcclient
 from transformers import AutoTokenizer
 from tritonclient.utils import InferenceServerException, np_to_triton_dtype
-import threading
+import multiprocessing
 
 
 def _input(name: str, data: np.ndarray) -> grpcclient.InferInput:
@@ -21,23 +21,27 @@ class TrtLLM:
         input_lengths = []
         for i in input_id:
             input_lengths.append(len(i))
-        import time
         with grpcclient.InferenceServerClient("localhost:8001", verbose=False) as client:
-            def send(client, tokenizer, model_name, input_id, input_length, max_new_tokens):
-                start = time.time()
-                inputs = [
-                    _input("input_ids", np.array(input_id, dtype=np.int32).reshape(1, -1)),
-                    _input("input_lengths", np.array([input_length], dtype=np.int32).reshape(1, -1)),
-                    _input("request_output_len", np.array([max_new_tokens], dtype=np.uint32).reshape(1, -1)),
-                    _input("end_id", np.array([2], dtype=np.uint32).reshape(1, -1)),
-                ]
-                output = client.infer(model_name, inputs).as_numpy("output_ids")
-                print(client.infer(model_name, inputs).as_numpy("sequence_length"))
-                print(output)
-                print(tokenizer.decode(output.reshape(-1)))
-                print(time.time()-start)
-            # Create and start n threads
-            send(client, self.tokenizer, 'tensorrt_llm', input_id[0], input_lengths[0], max_new_tokens)
+            with multiprocessing.Manager() as manager:
+                def send(client, input_id, input_length, i, shared_list):
+                    inputs = [
+                        _input("input_ids", np.array(input_id, dtype=np.int32).reshape(1, -1)),
+                        _input("input_lengths", np.array([input_length], dtype=np.int32).reshape(1, -1)),
+                        _input("request_output_len", np.array([max_new_tokens], dtype=np.uint32).reshape(1, -1)),
+                        _input("end_id", np.array([2], dtype=np.uint32).reshape(1, -1)),
+                    ]
+                    shared_list[i] = client.infer('tensorrt_llm', inputs).as_numpy('sequence_length').reshape(-1)[0]
+                processes = []
+                shared_list = manager.list([""] * batch_size)
+                for i in range(batch_size):
+                    process = multiprocessing.Process(target=send, args=(client, input_id[i], input_lengths[i], i, shared_list))
+                    processes.append(process)
+                    process.start()
+
+                for process in processes:
+                    process.join()
+        print(shared_list)
+            # send(client, self.tokenizer, 'tensorrt_llm', input_id[0], input_lengths[0], max_new_tokens)
 
         output_beams_list = [
             self.tokenizer.batch_decode(output_ids[batch_idx, :,
