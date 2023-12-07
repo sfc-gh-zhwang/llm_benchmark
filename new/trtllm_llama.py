80544250bd8bf6514fbb1a3ae3b7fd9319d31c72
sfc-gh-zhwang <flex.wang@snowflake.com>
2023-12-06 23:22:48 -0800

    commit

---
+++ new/trtllm_llama.py
@@ -0,0 +1,118 @@
+import tensorrt_llm
+import tensorrt_llm.profiler as profiler
+from tensorrt_llm.logger import logger
+from tensorrt_llm.quantization import QuantMode
+import json
+import os
+import torch
+from transformers import AutoTokenizer
+
+
+def get_engine_name(model, dtype, tp_size, pp_size, rank):
+    if pp_size == 1:
+        return '{}_{}_tp{}_rank{}.engine'.format(model, dtype, tp_size, rank)
+    return '{}_{}_tp{}_pp{}_rank{}.engine'.format(model, dtype, tp_size, pp_size, rank)
+
+
+def get_decoder(engine_dir):
+    config_path = os.path.join(engine_dir, 'config.json')
+    with open(config_path, 'r') as f:
+        config = json.load(f)
+    dtype = config['builder_config']['precision']
+    tp_size = config['builder_config']['tensor_parallel']
+    pp_size = config['builder_config']['pipeline_parallel']
+    world_size = tp_size * pp_size
+    assert world_size == tensorrt_llm.mpi_world_size(), \
+        f'Engine world size ({world_size}) != Runtime world size ({tensorrt_llm.mpi_world_size()})'
+    num_heads = config['builder_config']['num_heads'] // tp_size
+    hidden_size = config['builder_config']['hidden_size'] // tp_size
+    vocab_size = config['builder_config']['vocab_size']
+    num_layers = config['builder_config']['num_layers']
+    use_gpt_attention_plugin = bool(
+        config['plugin_config']['gpt_attention_plugin'])
+    remove_input_padding = config['plugin_config']['remove_input_padding']
+    num_kv_heads = config['builder_config'].get('num_kv_heads', num_heads)
+    paged_kv_cache = config['plugin_config']['paged_kv_cache']
+    tokens_per_block = config['plugin_config']['tokens_per_block']
+    use_custom_all_reduce = config['plugin_config'].get('use_custom_all_reduce',
+                                                        False)
+    quant_mode = QuantMode(config['builder_config']['quant_mode'])
+    if config['builder_config'].get('multi_query_mode', False):
+        tensorrt_llm.logger.warning(
+            "`multi_query_mode` config is deprecated. Please rebuild the engine."
+        )
+        num_kv_heads = 1
+    num_kv_heads = (num_kv_heads + tp_size - 1) // tp_size
+    model_config = tensorrt_llm.runtime.ModelConfig(
+        vocab_size=vocab_size,
+        num_layers=num_layers,
+        num_heads=num_heads,
+        num_kv_heads=num_kv_heads,
+        hidden_size=hidden_size,
+        paged_kv_cache=paged_kv_cache,
+        tokens_per_block=tokens_per_block,
+        gpt_attention_plugin=use_gpt_attention_plugin,
+        remove_input_padding=remove_input_padding,
+        use_custom_all_reduce=use_custom_all_reduce,
+        dtype=dtype,
+        quant_mode=quant_mode)
+    runtime_rank = tensorrt_llm.mpi_rank()
+    runtime_mapping = tensorrt_llm.Mapping(world_size,
+                                        runtime_rank,
+                                        tp_size=tp_size,
+                                        pp_size=pp_size)
+    torch.cuda.set_device(runtime_rank % runtime_mapping.gpus_per_node)
+    engine_name = get_engine_name('llama', dtype, tp_size, pp_size,
+                                runtime_rank)
+    serialize_path = os.path.join(engine_dir, engine_name)
+    tensorrt_llm.logger.set_level('info')
+    profiler.start('load tensorrt_llm engine')
+    with open(serialize_path, 'rb') as f:
+        engine_buffer = f.read()
+    decoder = tensorrt_llm.runtime.GenerationSession(model_config,
+                                                    engine_buffer,
+                                                    runtime_mapping)
+    profiler.stop('load tensorrt_llm engine')
+    tensorrt_llm.logger.info(
+        f'Load engine takes: {profiler.elapsed_time_in_sec("load tensorrt_llm engine")} sec'
+    )
+    return decoder
+
+
+class TrtLLM:
+    def __init__(self, engine_dir, tokenizer_dir):
+        self.decoder = get_decoder(engine_dir=engine_dir)
+        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_dir)
+
+    def generate(self, prompts):
+        batch_size = len(prompts)
+        if len(prompts) == 1:
+            prompts = prompts[0]
+        input_id = self.tokenizer(prompts, padding=False).input_ids
+        input_lengths = []
+        for i in input_id:
+            input_lengths.append(len(i))
+        max_length = max(input_lengths)
+        line_encoded = [
+            torch.tensor(t, dtype=torch.int32).cuda() for t in input_id
+        ]
+        print(line_encoded)
+        input_lengths = torch.tensor(input_lengths, dtype=torch.int32).cuda()
+        sampling_config = tensorrt_llm.runtime.SamplingConfig(end_id=2,
+                                                              pad_id=0)
+        print(sampling_config)
+        with torch.no_grad():
+            self.decoder.setup(batch_size=batch_size,
+                               max_context_length=max_length,
+                               max_new_tokens=1024)
+            output_ids = self.decoder.decode_batch(
+                line_encoded, sampling_config)
+        output_beams_list = [
+            self.tokenizer.batch_decode(output_ids[batch_idx, :,
+                                                   input_lengths[batch_idx]:],
+                                                   skip_special_tokens=True)
+            for batch_idx in range(batch_size)
+        ]
+        for i in output_beams_list:
+            print(i)
+        return prompts
---
+++ new/trtllm_triton.py
@@ -0,0 +1,51 @@
+import numpy as np
+import tritonclient.grpc as grpcclient
+from transformers import AutoTokenizer
+from tritonclient.utils import InferenceServerException, np_to_triton_dtype
+import multiprocessing
+import time
+
+
+def _input(name: str, data: np.ndarray) -> grpcclient.InferInput:
+    t = grpcclient.InferInput(name, data.shape, np_to_triton_dtype(data.dtype))
+    t.set_data_from_numpy(data)
+    return t
+
+
+class TrtLLM:
+    def __init__(self, tokenizer_dir):
+        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_dir)
+
+    def generate(self, prompts, max_new_tokens):
+        batch_size = len(prompts)
+        input_id = self.tokenizer(prompts,
+                                  padding=False).input_ids
+        input_lengths = []
+        output_lengths = []
+        for i in input_id:
+            input_lengths.append(len(i))
+        with multiprocessing.Manager() as manager:
+            def send(client, input_id, input_length, i, shared_list):
+                inputs = [
+                    _input("input_ids", np.array(input_id, dtype=np.int32).reshape(1, -1)),
+                    _input("input_lengths", np.array([input_length], dtype=np.int32).reshape(1, -1)),
+                    _input("request_output_len", np.array([max_new_tokens], dtype=np.uint32).reshape(1, -1)),
+                    _input("end_id", np.array([2], dtype=np.uint32).reshape(1, -1)),
+                ]
+                with grpcclient.InferenceServerClient("localhost:8001", verbose=False) as client:
+                    print('send')
+                    shared_list[i] = client.infer('tensorrt_llm', inputs).as_numpy('output_ids').reshape(-1).shape[0]
+            processes = []
+            shared_list = manager.list([""] * batch_size)
+            start = time.time()
+            for i in range(batch_size):
+                process = multiprocessing.Process(target=send, args=(None, input_id[i], input_lengths[i], i, shared_list))
+                processes.append(process)
+                process.start()
+
+            for process in processes:
+                process.join()
+            latency = time.time() - start
+            for i in shared_list:
+                output_lengths.append(i)
+        return latency, input_lengths, output_lengths
