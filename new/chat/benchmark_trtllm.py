acd42076117f3ef3581e7c25bf7dbd7ab8832404
sfc-gh-zhwang <flex.wang@snowflake.com>
2023-12-10 17:32:18 -0800

    commit

--- new/chat/benchmark_trtllm.py
+++ new/chat/benchmark_trtllm.py
@@ -112,6 +112,7 @@ def benchmark_vllm(
         _input("input_lengths", np.array(input_lengths, dtype=np.int32).reshape(1, -1)),
         _input("request_output_len", np.array([max_new_tokens], dtype=np.uint32).reshape(1, -1)),
         _input("end_id", np.array([2], dtype=np.uint32).reshape(1, -1)),
+        _input("streaming", np.array([True], dtype=np.bool_).reshape(1, -1)),
     ]
     # inputs = [
     #     _input("text", np.array(query.prompt, dtype=object).reshape(1, -1)),
