cf94f47d55079d992bde28d54c28dfc2fca2cc30
sfc-gh-zhwang <flex.wang@snowflake.com>
2023-12-10 17:26:52 -0800

    commit

--- new/chat/benchmark_trtllm.py
+++ new/chat/benchmark_trtllm.py
@@ -98,7 +98,7 @@ def benchmark_vllm(
     def stream_callback(a, result, error):
         if error:
             print(error)
-        a.put((result.as_numpy('output_sequence_lengths').reshape(-1,)[0], time.time(), result.as_numpy('output_ids').reshape(-1,)[0]))
+        a.put((result.as_numpy('sequence_length').reshape(-1,)[0], time.time(), result.as_numpy('output_ids').reshape(-1,)[0]))
 
     tokenizer = AutoTokenizer.from_pretrained(model)
     input_id = tokenizer(prompts, padding=False).input_ids
